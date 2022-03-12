acbc9e690c73c005466f66902cdb68892309e3f4
embunazzahra <dhauembunazzahra@gmail.com>
2022-03-12 13:11:06 +0700

    NODE_ENV

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const message = (name) => {
-    console.log(`Hello ${name}`);
- }
-  
- message('Javascript');
+//implementasi process.env
+//untuk membuat host yg berbeda di antara development dan production
+//menggunakan properti NODE_ENV
+
+const server = new Server(
+    {
+        host: process.env.NODE_ENV !== 'production'?'localhost':'dicoding.com',
+    }
+);
+
+//CMD command to assign NODE_ENV:
+//SET NODE_ENV=production && node [namafile].js
